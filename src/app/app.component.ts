b62377bb2716004ec94936ca9553197d5e795272
athulkrishnaph <athulhassi@gmail.com>
2025-05-22 19:02:45 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,24 +9,7 @@ import { map } from 'rxjs/operators';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet],
-  template: `
-    <nav class="navbar navbar-expand navbar-dark bg-primary mb-4">
-      <div class="container">
-        <a class="navbar-brand" routerLink="">ShopCart</a>
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link" routerLink="">Products</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" routerLink="cart">Cart ({{ totalItems$ | async }})</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-    <div class="container">
-      <router-outlet></router-outlet>
-    </div>
-  `
+  templateUrl: `app.component.html`
 })
 export class AppComponent implements OnInit, OnDestroy {
   totalItems$: Observable<number>;
